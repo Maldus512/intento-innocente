34e1c5e0369c83023b4cb68a608194f02f9a89e7
Mattia Maldini <mattia512maldini@gmail.com>
2021-12-13 08:23:31 +0100

    Aggiunti ingressi, corretto errore su rele 7

--- main/main.c
+++ main/main.c
@@ -7,10 +7,15 @@
 #include "peripherals/dipswitch.h"
 #include "lightmodbus/lightmodbus.h"
 #include "lightmodbus/slave.h"
+#include "peripherals/digin.h"
+#include "gel/serializer/serializer.h"
 
 
 int main(void) {
+    unsigned long timestamp = 0;
+    
     system_init();
+    digin_init();
     digout_init();
     timer_init();
     uart_init();
@@ -20,33 +25,38 @@ int main(void) {
     
     ModbusSlave minion={0};
     uint8_t coils[2] = {0};
+    uint8_t discrete[2] = {0};
     minion.coils = coils;
     minion.coilCount = 16;
+    minion.discreteInputs = discrete;
+    minion.discreteInputCount = 12;
     minion.address = 2 + config;
     modbusSlaveInit(&minion);
     
-    unsigned long tsrx = 0;
     int i = 0;
     
     for (;;) {
-        if (is_expired(tsrx,get_millis(), 10))  {
-           int len = uart_read_rx_buffer(minion.request.frame); 
-           minion.request.length=len;
-           
-           if (len > 0) {
-                ModbusError err = modbusParseRequest(&minion);
-                if (err==MODBUS_OK || err==MODBUS_ERROR_EXCEPTION) {
-                    uart_clean_rx_buffer();
-                    uart_sync_write(minion.response.frame, minion.response.length);
-                }
+       int len = uart_read_rx_buffer(minion.request.frame); 
+       minion.request.length=len;
+
+       if (len > 0) {
+            ModbusError err = modbusParseRequest(&minion);
+            if (err==MODBUS_OK || err==MODBUS_ERROR_EXCEPTION) {
+                uart_clean_rx_buffer();
+                uart_sync_write(minion.response.frame, minion.response.length);
 
                 for (i = 0; i < NUM_RELE; i++) {
                     digout_update_rele(i, (coils[i/8] & (1 << (i % 8))) > 0);
                 }
+            }
+       }
+       
+       if (is_expired(timestamp, get_millis(), 10)) {
+           if (digin_take_reading() > 0) {
+               serialize_uint16_le(discrete, (uint16_t)digin_get_inputs());
            }
-           
-           tsrx=get_millis();
-        }
+           timestamp = get_millis();
+       }
         
         Idle(); // This also resets the watchdog timer
     }
